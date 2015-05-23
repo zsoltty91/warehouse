087aab5c59682bb972256e417cf3f798353b8674
Lámfalusi Csaba <csaba.lamfalusi@neuron.hu>
2015-05-23 17:50:52 +0200

    Menüpontok felrakása

--- application/warehouse-web/src/main/java/hu/neuron/java/warehouse/web/controller/WarehouseController.java
+++ application/warehouse-web/src/main/java/hu/neuron/java/warehouse/web/controller/WarehouseController.java
@@ -9,7 +9,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 @ViewScoped
